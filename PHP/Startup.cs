772eaa473033638d6d77a310193deed7faeeb915
dkumpa <dalius.kumpa@gmail.com>
2018-01-26 14:29:16 -0800

    s

--- PHP/Startup.cs
+++ PHP/Startup.cs
@@ -57,7 +57,7 @@ namespace PHP
             app.UseSession();
             app.UseAuthentication();
             IdentitySeedData.EnsurePopulated(app);
-            // adding test 2
+            // 
             app.UseMvc(routes =>
             {
 
